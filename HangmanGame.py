7ec5f114f189fd8ee2c0aa10b77d40d2c7d4a525
anikmaur <aniket.maurya@gmail.com>
2018-10-01 15:07:32 +0530

    Condition updated

--- HangmanGame.py
+++ HangmanGame.py
@@ -1,54 +1,62 @@
-import string
-
-
 def verifyInput(guessedLetter, vowels, guessedLetterList):
-    if (guessedLetter.isalpha() and len(guessedLetter) == 1) and (guessedLetter not in guessedLetterList) and (guessedLetter not in vowels):
-        return True
-    else:
-        print("error in input")
+    if not (guessedLetter.isalpha() and len(guessedLetter) == 1):
+        print("Please enter a single alphabet")
         return False
-
-while True:
-    word = input("Enter word to guess:").upper()
-    if word.isalpha() and len(word) >= 5:
-        break
+    elif guessedLetter in guessedLetterList:
+        print("You have already guessed", guessedLetter)
+        return False
+    elif guessedLetter in vowels:
+        print("Please enter a consonant")
+        return False
     else:
-        print("Please enter at least a 5 letters long word without numbers or special character")
+        return True
 
-vowels = ["A", "E", "I", "O", "U"]
-numberOfUniqueLetterToGuess = []
-for i in word:
-    if i in vowels:
-        print(i, end=" ")
-    else:
-        print("_", end=" ")
-        if i not in numberOfUniqueLetterToGuess:
-            numberOfUniqueLetterToGuess.append(i)
-print()
-print(numberOfUniqueLetterToGuess)
-guessedLetterList = []
-hangman = 5
-while hangman > 0 and (len(guessedLetterList) != len(numberOfUniqueLetterToGuess)):
+
+def hangmanGame():
     while True:
-        guessedLetter = input("Guess a consonant letter:").upper()
-        if verifyInput(guessedLetter, vowels, guessedLetterList):
+        word = input("Enter word to guess:").upper()
+        if word.isalpha() and len(word) >= 5:
             break
+        else:
+            print("Please enter at least a 5 letters long word without numbers or special character")
 
-    if guessedLetter in word:
-        guessedLetterList.append(guessedLetter)
-        for i in word:
-            if i in vowels or i in guessedLetterList:
-                print(i, end=" ")
-            else:
-                print("_", end=" ")
-    else:
-        chancesLeft = hangman - 1
-        hangman -= 1
-        print(chancesLeft, "guesses left")
+    vowels = ["A", "E", "I", "O", "U"]
+    numberOfUniqueLetterToGuess = []
+    for i in word:
+        if i in vowels:
+            print(i, end=" ")
+        else:
+            print("_", end=" ")
+            if i not in numberOfUniqueLetterToGuess:
+                numberOfUniqueLetterToGuess.append(i)
     print()
-    print(guessedLetterList)
-if hangman == 0:
-    print("You lost ;/")
-else:
-    print("Congratulations!!! You won :-)")
+    #print(numberOfUniqueLetterToGuess)
+    guessedLetterList = []
+    hangman = 5
+    while hangman > 0 and (len(guessedLetterList) != len(numberOfUniqueLetterToGuess)):
+        while True:
+            guessedLetter = input("Guess a letter:").upper()
+            if verifyInput(guessedLetter, vowels, guessedLetterList):
+                break
 
+        if guessedLetter in word:
+            guessedLetterList.append(guessedLetter)
+            for i in word:
+                if i in vowels or i in guessedLetterList:
+                    print(i, end=" ")
+                else:
+                    print("_", end=" ")
+        else:
+            chancesLeft = hangman - 1
+            hangman -= 1
+            print(chancesLeft, "guesses left")
+        print()
+        #print(guessedLetterList)
+    if hangman == 0:
+        print("You lost -_-")
+    else:
+        print("Congratulations!!! You won :-)")
+
+
+if __name__ == "__main__":
+    hangmanGame()
